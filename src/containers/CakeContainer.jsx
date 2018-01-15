c28328268dc4507879e2af782214f0bb8b0fabb9
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-01-15 14:54:18 +0000

    API link set up

--- src/containers/CakeContainer.jsx
+++ src/containers/CakeContainer.jsx
@@ -9,6 +9,23 @@ class CakeContainer extends React.Component {
     }
   }
 
+  componentDidMount(){
+    const url = 'http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/cakes';
+    const request = new XMLHttpRequest();
+    request.open("GET", url);
+    request.addEventListener('load', () => {
+      if (request.status === 200) {
+        const jsonString = request.responseText;
+        const data = JSON.parse(jsonString);
+        this.setState({ countries: data }, () => {
+          this.buildCountryArray(this.state.randomNumber)
+        });
+      }
+    });
+    request.send();
+  }
+
+
   render() {
     return(
       <div>Cake list goes here</div>
