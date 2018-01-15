9a4f506f141f78b683839fea46e5af64fb9298cf
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-01-15 21:20:10 +0000

    display text button

--- src/containers/CakeContainer.jsx
+++ src/containers/CakeContainer.jsx
@@ -8,10 +8,13 @@ class CakeContainer extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      cakes_data: []
+      cakes_data: [],
+      display: "none"
     };
     this.handleClick = this.handleClick.bind(this);
+    this.clickCake = this.clickCake.bind(this);
   }
+
 
   componentDidMount(){
     const url = 'http://ec2-52-209-201-89.eu-west-1.compute.amazonaws.com:5000/api/cakes';
@@ -32,11 +35,28 @@ class CakeContainer extends React.Component {
     return <Redirect to={cakeLink} />
   }
 
+  clickCake(event){
+    this.setState({display: "block"})
+  }
+
   render() {
 
     const cakes = this.state.cakes_data.map((cake, index) => {
       return <div className="cake">
         <img alt="Picture of a cake!" src={cake.imageUrl}></img>
+        <div className="cake-details">
+
+          <h2>{cake.name}</h2>
+          <button onClick={this.clickCake}>Show details!</button>
+          <p style={{display: this.state.display}}>{cake.comment}</p>
+          <p style={{display: this.state.display}}>Yum rating: {cake.yumFactor}</p>
+        </div>
+      </div>
+    });
+
+    const cakes_details = this.state.cakes_data.map((cake, index) => {
+      return <div className="cake">
+        <img alt="Picture of a cake!" src={cake.imageUrl} onClick={this.clickCake}></img>
         <div className="cake-details">
 
           <h2 onClick={this.handleClick}>{cake.name}</h2>
