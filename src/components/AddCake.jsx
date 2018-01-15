d3b2173061e15cfc248335479b8847da871cfcaf
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-01-15 19:46:12 +0000

    add form constructed

--- src/components/AddCake.jsx
+++ src/components/AddCake.jsx
@@ -13,8 +13,31 @@ class AddCake extends React.Component {
 
 
   render() {
+
     return (
-      <div>hello world</div>
+      <form onSubmit={this.handleSubmit}>
+        Name: <input name="name" type="text" placeholder="Cake name..."/>
+        Comment: <textarea name="Comment..."/>
+        Cake pic url URL: <input type="text" placeholder="Image URL"/>
+
+        <div className="radio">
+          <label><input type="radio" value="yum1" name="yumRating" />1</label>
+        </div>
+        <div className="radio">
+          <label><input type="radio" value="yum2" name="yumRating" />2</label>
+        </div>
+        <div className="radio">
+          <label><input type="radio" value="yum3" name="yumRating" />3</label>
+        </div>
+        <div className="radio">
+          <label><input type="radio" value="yum4" name="yumRating" />4</label>
+        </div>
+        <div className="radio">
+          <label><input type="radio" value="yum5" name="yumRating" />5</label>
+        </div>
+        <button type="submit" value="Add">Submit</button>
+
+      </form>
     );
   }
 };
