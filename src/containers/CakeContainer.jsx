c3f97cfcee8de9bf8233c016d27d944ac6420cc8
James Stewart <james.stewart.06@aberdeen.ac.uk>
2018-01-15 14:51:56 +0000

    CakeContainer constructor set up

--- src/containers/CakeContainer.jsx
+++ src/containers/CakeContainer.jsx
@@ -1,6 +1,11 @@
 import React from 'react';
 
 class CakeContainer extends React.Component {
+
+  constructor(super){
+    super(props);
+  }
+
   render() {
     return(
       <div>Cake list goes here</div>
